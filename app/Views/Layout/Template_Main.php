9337a0ac04f0af95a2b3590701727e9f7918c946
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-06-30 21:30:09 +0700

    fix style and js

--- app/Views/Layout/Template_Main.php
+++ app/Views/Layout/Template_Main.php
@@ -9,40 +9,40 @@
     <!-- Google Font: Source Sans Pro -->
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
-    <!-- <link rel="stylesheet" href="/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css"> -->
+    <!-- <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css"> -->
     <!-- JQVMap -->
-    <link rel="stylesheet" href="/assets/plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/jqvmap/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="/assets/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/dist/css/adminlte.min.css">
     <!-- overlayScrollbars -->
-    <!-- <link rel="stylesheet" href="/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"> -->
+    <!-- <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"> -->
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="/assets/plugins/summernote/summernote-bs4.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/summernote/summernote-bs4.min.css">
     <!-- DataTables -->
-    <link rel="stylesheet" href="/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     <!-- Select2 -->
-    <link rel="stylesheet" href="/assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="/assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
     <!-- daterange picker -->
-    <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/daterangepicker/daterangepicker.css">
     <!-- Select2 -->
-    <link rel="stylesheet" href="/assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="/assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
     <!-- daterange picker -->
-    <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/daterangepicker/daterangepicker.css">
     <!-- daterange picker -->
-    <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
-    <!-- <link rel="stylesheet" href="/assets/css/styles.css"> -->
+    <link rel="stylesheet" href="http://localhost/cycletime/assets/plugins/daterangepicker/daterangepicker.css">
+    <!-- <link rel="stylesheet" href="http://localhost/cycletime/assets/css/styles.css"> -->
     <!-- jQuery -->
     <!-- <script src="http://localhost/cycletime/assets/plugins/jquery/jquery.min.js"></script> -->
 </head>
