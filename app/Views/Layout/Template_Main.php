c860b7faa71ccd8c3f7c16cbd26fddb252019d77
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-06-30 21:29:44 +0700

    fix style and js

--- app/Views/Layout/Template_Main.php
+++ app/Views/Layout/Template_Main.php
@@ -44,7 +44,7 @@
     <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
     <!-- <link rel="stylesheet" href="/assets/css/styles.css"> -->
     <!-- jQuery -->
-    <!-- <script src="/assets/plugins/jquery/jquery.min.js"></script> -->
+    <!-- <script src="http://localhost/cycletime/assets/plugins/jquery/jquery.min.js"></script> -->
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
@@ -82,7 +82,7 @@
         <aside class="main-sidebar sidebar-light-olive elevation-1">
             <!-- Brand Logo -->
             <a href="/" class="brand-link bg-light">
-                <img src="/assets/dist/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-1" style="opacity: 1">
+                <img src="http://localhost/cycletime/assets/dist/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-1" style="opacity: 1">
                 <span class="brand-text font-weight-light">Cycletime Report</span>
             </a>
 
@@ -178,57 +178,57 @@
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="/assets/plugins/jquery/jquery.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <!-- <script src="/assets/plugins/chart.js/Chart.min.js"></script> -->
+    <!-- <script src="http://localhost/cycletime/assets/plugins/chart.js/Chart.min.js"></script> -->
     <!-- Sparkline -->
-    <!-- <script src="/assets/plugins/sparklines/sparkline.js"></script> -->
+    <!-- <script src="http://localhost/cycletime/assets/plugins/sparklines/sparkline.js"></script> -->
     <!-- JQVMap -->
-    <script src="/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <!-- <script src="/assets/plugins/jquery-knob/jquery.knob.min.js"></script> -->
+    <!-- <script src="http://localhost/cycletime/assets/plugins/jquery-knob/jquery.knob.min.js"></script> -->
     <!-- daterangepicker -->
-    <script src="/assets/plugins/moment/moment.min.js"></script>
-    <script src="/assets/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/moment/moment.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="/assets/plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <!-- <script src="/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
+    <!-- <script src="http://localhost/cycletime/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
     <!-- AdminLTE App -->
-    <script src="/assets/dist/js/adminlte.js"></script>
+    <script src="http://localhost/cycletime/assets/dist/js/adminlte.js"></script>
     <!-- DataTables  & Plugins -->
-    <script src="/assets/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="/assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-    <script src="/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-    <script src="/assets/plugins/jszip/jszip.min.js"></script>
-    <script src="/assets/plugins/pdfmake/pdfmake.min.js"></script>
-    <script src="/assets/plugins/pdfmake/vfs_fonts.js"></script>
-    <script src="/assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-    <script src="/assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-    <script src="/assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/jszip/jszip.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
     <!-- Select2 -->
-    <script src="/assets/plugins/select2/js/select2.full.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/select2/js/select2.full.min.js"></script>
     <!-- InputMask -->
-    <!-- <script src="/assets/plugins/moment/moment.min.js"></script>
-    <script src="/assets/plugins/inputmask/jquery.inputmask.min.js"></script> -->
+    <!-- <script src="http://localhost/cycletime/assets/plugins/moment/moment.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/inputmask/jquery.inputmask.min.js"></script> -->
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- date-range-picker -->
-    <script src="/assets/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="http://localhost/cycletime/assets/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Custom Script -->
     <script>
         // Daterange
